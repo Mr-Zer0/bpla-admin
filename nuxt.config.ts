39da9c708620cdcc1d6766f42e16e876b2f858ab
Yan Naing <tannaing@gmail.com>
2023-08-08 23:06:16 +0630

    Add inter web font from google fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  pages: true,
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {}
+    }
+  },
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com'
+        },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: ''
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap'
+        }
+      ]
     }
   }
 })
