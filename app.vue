f7a379682909dbb262c033b9a1152f92d98e1f89
Yan Naing <tannaing@gmail.com>
2023-08-17 11:07:42 +0630

    Activate layout system

--- app.vue
+++ app.vue
@@ -1,19 +1,7 @@
 <template>
-  <div class="bg-slate-100" id="bpla">
-    <TopBar />
+  <NuxtLayout>
 
-    <NuxtPage/>
-  </div>
+    <NuxtPage />
+
+  </NuxtLayout>
 </template>
-
-<script lang="ts">
-export default {
-  mounted() {
-    const bpla_element = document.getElementById('bpla')
-    
-    if (bpla_element != null) {
-      bpla_element.style.minHeight = window.innerHeight + 'px'
-    }
-  }
-}
-</script>
---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg-slate-100" id="bpla">
+    <TopBar />
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  mounted() {
+    const bpla_element = document.getElementById('bpla')
+
+    if (bpla_element != null) {
+      bpla_element.style.minHeight = window.innerHeight + 'px'
+    }
+  }
+}
+</script>
