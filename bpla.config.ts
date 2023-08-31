3c6756eb59fb405c709a3aad34a1bcd0718450cb
Yan Naing <tannaing@gmail.com>
2023-08-31 23:42:28 +0630

    Refector:Change from nuxt 3 to vue 3

--- bpla.config.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  name: 'Burma People\'s Libration Army',
-  name_short: 'BPLA',
-  name_mm: 'ဗမာပြည်သူ့လွတ်မြောက်ရေးတပ်တော်',
-  slogan: 'တိုက်ပွဲ၊​ အောင်ပွဲမှသည် လူထုလွတ်မြောက်ရေးဆီသို့'
-}
