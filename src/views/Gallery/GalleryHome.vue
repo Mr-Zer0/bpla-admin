6aaf2f63151174318835635a2d475cfc6457b79a
Yan Naing <tannaing@gmail.com>
2023-10-09 22:30:45 +0630

    Added gallery home

--- src/views/Gallery/GalleryHome.vue
+++ src/views/Gallery/GalleryHome.vue
@@ -9,4 +9,59 @@
       Create
     </router-link>
   </section>
+
+  <section class="bg-white mt-5 rounded-lg border border-solid border-slate-200">
+
+    <div
+      v-for="(gallery, i) in galleries"
+      :key="gallery.id"
+      :class="[
+        i === galleries.length - 1 ? '' : 'border-b border-solid border-slate-200',
+        'px-5 py-3 flex justify-between'
+      ]"
+    >
+      <div class="flex items-center gap-x-2.5">
+        <h3 v-text="gallery.title" class="text-lg" />
+        <p
+          :class="[
+            gallery.status === 'drafted' ? 'text-slate-400' : 'text-green-600',
+            'text-xs uppercase font-semibold mt-1'
+          ]"
+          v-text="gallery.status"
+        />
+      </div>
+
+      <div class="flex flex-wrap md:flex-nowrap items-start gap-2">
+        <router-link :to="'/categories/edit/' + gallery.id">
+          <PencilSquareIcon class="w-5 text-slate-500 hover:text-slate-700" />
+        </router-link>
+        <a href=""> </a>
+        <button @click="remove(gallery)">
+          <TrashIcon class="w-5 h-5 text-slate-500 hover:text-slate-700" />
+        </button>
+      </div>
+    </div>
+
+  </section>
 </template>
+
+<script setup lang="ts">
+import type GalleryType from '@/contracts/gallery.interface';
+import { useGalleryStore } from '@/stores/gallery';
+import { PencilSquareIcon, TrashIcon } from '@heroicons/vue/24/outline'
+import { onMounted } from 'vue';
+import { ref } from 'vue';
+
+const galleryStore = useGalleryStore()
+
+const galleries = ref<Array<GalleryType>>([])
+
+onMounted(async () => {
+  galleries.value = await galleryStore.fetch()
+})
+
+const remove = (gallery: GalleryType) => {
+  console.log(gallery)
+}
+
+</script>
