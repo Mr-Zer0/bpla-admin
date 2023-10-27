d507214952aa23bb8b8a34c61ad6bd582c452d0f
Yan Naing <tannaing@gmail.com>
2023-10-27 21:50:03 +0630

    Add form input form component

---
+++ src/components/Form/InputElement.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="col-span-full">
+
+    <label
+      :for="name" 
+      class="block text-sm font-medium leading-6 text-gray-900 font-semibold"
+      v-text="props.label"
+    />
+
+    <div class="mt-2">
+      <input
+        :name="name"
+        :type="type"
+        :value="props.modelValue"
+        :required="props.required"
+        @input="input"
+        class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const props = withDefaults(defineProps<{
+  label: string
+  required?: boolean
+  modelValue?: string
+  type?: string
+  name?: string
+}>(), {
+  required: false,
+  type: 'text'
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const input = (e: Event) => {
+  emit('update:modelValue', (e.target as HTMLInputElement).value)
+}
+</script>
